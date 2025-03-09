8d3940ae152ce1d2b1a250ac7a4cdcf51557e9c8
VaidehiPatel1893 <142615160+VaidehiPatel1893@users.noreply.github.com>
2025-03-09 16:20:28 +0530

    Update RolePermissionViewModel.cs

--- Entity/ViewModel/RolePermissionViewModel.cs
+++ Entity/ViewModel/RolePermissionViewModel.cs
@@ -4,22 +4,13 @@ using System.ComponentModel.DataAnnotations;
 
 public class RolePermissionViewModel
 {
-    [Required(ErrorMessage = "Role Permission Id is required.")]
     public int Id { get; set; }
-    [Required(ErrorMessage = "Role Id is required.")]
     public int? RoleId { get; set; }
-    [Required(ErrorMessage = "Role Name is required.")]
-    [StringLength(100, ErrorMessage = "Role Name cannot exceed 100 characters.")]
     public string RoleName { get; set; } = null!;
-    [Required(ErrorMessage = "PErmission Id is required.")]
     public int? Permissionid { get; set; }
-    [StringLength(100, ErrorMessage = "Permission Name cannot exceed 100 characters.")]
     public string? PermissionName { get; set; }
-    [Required(ErrorMessage = "Can View field is required.")]
     public bool CanView { get; set; }
-    [Required(ErrorMessage = "Can add/Edit Field is required.")]
     public bool CanEdit { get; set; }
-    [Required(ErrorMessage = "Can Delete is required.")]
     public bool CanDelete { get; set; }
 }
 
