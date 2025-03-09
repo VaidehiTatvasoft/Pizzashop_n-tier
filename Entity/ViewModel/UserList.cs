023a85a48f00d4276272c151d2c72933b9b2af80
VaidehiPatel1893 <142615160+VaidehiPatel1893@users.noreply.github.com>
2025-03-09 16:21:24 +0530

    Update UserList.cs

--- Entity/ViewModel/UserList.cs
+++ Entity/ViewModel/UserList.cs
@@ -5,20 +5,9 @@ namespace Entity.ViewModel;
 public class UserList
 {
       public int Id { get; set; }
-      [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
-
     public string? Name { get; set; }
-        [EmailAddress(ErrorMessage = "Invalid email format.")]
     public string? Email { get; set; }
-        [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid phone number format.")]
-
     public long Phone { get; set; }
-        [StringLength(100, ErrorMessage = "Role Name cannot exceed 100 characters.")]
-
     public string? RoleName {get; set;}
-        [StringLength(50, ErrorMessage = "Status cannot exceed 50 characters.")]
-
     public string? Status { get; set; }
-
-
 }
