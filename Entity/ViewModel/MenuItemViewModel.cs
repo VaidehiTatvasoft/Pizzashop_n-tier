0a2e427b56708988dd734f91aa67602d5ef63fbd
vpatel-agilic <vpatel.agilic@gmail.com>
2025-03-15 16:22:45 +0530

    changes

--- Entity/ViewModel/MenuItemViewModel.cs
+++ Entity/ViewModel/MenuItemViewModel.cs
@@ -4,24 +4,15 @@ namespace Entity.ViewModel;
 
 public class MenuItemViewModel
     {
-        public int? Id { get; set; }
-        [Required(ErrorMessage = "Name is required.")]
-        public string Name { get; set; } = null!;
-        [Required(ErrorMessage = "Category is required.")]
-        public int CategoryId { get; set; }
-        public bool Type { get; set; }
-        public decimal Rate { get; set; }
-        public int? Quantity { get; set; }
-        public int UnitId { get; set; }
-        public bool IsAvailable { get; set; }
-        public bool? IsFavourite { get; set; }
-        public bool? IsDefaultTax { get; set; }
-        public decimal? TaxPercentage { get; set; }
-        public string? ShortCode { get; set; }
-        public string? Description { get; set; }
-        public bool? IsDeleted { get; set; }
-        public List<int> ModifierGroupId { get; set; } = new List<int>();
-        public List<ModifierGroupViewModel> ModifierGroups { get; set; }= new List<ModifierGroupViewModel>();
-        public string RemovedGroups { get; set; }
+        public class MenuItemViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool Type { get; set; } 
+    public decimal Rate { get; set; }
+    public int Quantity { get; set; }
+    public bool IsAvailable { get; set; }
+    public List<int> ModifierIds { get; set; }
+}
 
     }
