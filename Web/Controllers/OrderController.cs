37e35ba79a7d9bd2fa2c2ec687b742eb03a24a07
VaidehiTatvasoft <tatva.pce54@outlook.com>
2025-03-26 16:05:01 +0530

    Orders changes

--- Web/Controllers/OrderController.cs
+++ Web/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Service.Interface;
 using System.Linq;
 using System.Threading.Tasks;
+using Web.Attributes;
 namespace Web.Controllers;
 
 public class OrderController : Controller
@@ -15,35 +16,29 @@ public class OrderController : Controller
     {
         _orderService = orderService;
     }
+    [CustomAuthorize(1, RolePermissionEnum.Permission.CanView)]
 
     public IActionResult Order(string searchTerm, int pageIndex = 1, int pageSize = 10, string sortOrder = "", bool isAjax = false)
+    {
+        var orders = _orderService.GetAllOrderViewModels(searchTerm, sortOrder, pageIndex, pageSize, out int count);
+
+        ViewData["OrderIdSortParam"] = sortOrder == "orderid_asc" ? "orderid_desc" : "orderid_asc";
+        ViewData["DateSortParam"] = sortOrder == "date_asc" ? "date_desc" : "date_asc";
+        ViewData["CustomerNameSortParam"] = sortOrder == "customername_asc" ? "customername_desc" : "customername_asc";
+
+        ViewBag.Count = count;
+        ViewBag.PageIndex = pageIndex;
+        ViewBag.PageSize = pageSize;
+        ViewBag.TotalPage = (int)Math.Ceiling(count / (double)pageSize);
+        ViewBag.SearchTerm = searchTerm;
+        ViewBag.SortOrder = sortOrder;
+
+        if (isAjax)
         {
-            var orders = _orderService.GetAllOrderViewModels(searchTerm, sortOrder, pageIndex, pageSize, out int count);
-
-            ViewData["OrderIdSortParam"] = sortOrder == "orderid_asc" ? "orderid_desc" : "orderid_asc";
-            ViewData["DateSortParam"] = sortOrder == "date_asc" ? "date_desc" : "date_asc";
-            ViewData["CustomerNameSortParam"] = sortOrder == "customername_asc" ? "customername_desc" : "customername_asc";
-
-            ViewBag.Count = count;
-            ViewBag.PageIndex = pageIndex;
-            ViewBag.PageSize = pageSize;
-            ViewBag.TotalPage = (int)Math.Ceiling(count / (double)pageSize);
-            ViewBag.SearchTerm = searchTerm;
-            ViewBag.SortOrder = sortOrder;
-
-            if (orders == null || !orders.Any())
-            {
-                ViewBag.ErrorMessage = "Order list is empty.";
-            }
-
-            ViewBag.OrderList = orders;
-
-            if (isAjax)
-            {
-                return PartialView("_OrderList", orders);
-            }
-
-            return View(orders);
+            return PartialView("_OrderList");
         }
+
+        return View(Order);
+    }
 
 }
